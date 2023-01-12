91de9ee3886b499165e1747fc0afa7ef1fc491f4
Martin Štiglinc <martin.stiglinc@gmail.com>
2023-01-12 22:41:31 +0100

    Add files via upload

---
+++ score.js
@@ -0,0 +1,60 @@
+let scoreGuest = 0
+let scoreHome = 0
+let guestEl = document.getElementById("guest-el")
+let homeEl = document.getElementById("home-el")
+
+function addG1(){
+    scoreGuest += 1
+    guestEl.textContent = scoreGuest
+    colors()
+}
+
+function addG2(){
+    scoreGuest += 2
+    guestEl.textContent = scoreGuest
+    colors()
+}
+function addG3(){
+    scoreGuest += 3
+    guestEl.textContent = scoreGuest
+    colors()
+}
+function addH1(){
+    scoreHome += 1
+    homeEl.textContent = scoreHome
+    colors()
+}
+function addH2(){
+    scoreHome += 2
+    homeEl.textContent = scoreHome
+    colors()
+}
+function addH3(){
+    scoreHome += 3
+    homeEl.textContent = scoreHome
+    colors()
+}
+
+function colors(){
+    if(scoreHome>scoreGuest){
+        homeEl.style.color = "green"
+        guestEl.style.color = "#CC360F"
+    }
+    else if (scoreHome<scoreGuest){
+        homeEl.style.color = "#CC360F"
+        guestEl.style.color = "green"
+    }
+    else {
+        homeEl.style.color = "#CC360F"
+        guestEl.style.color = "#CC360F"
+    }
+}
+
+function nullScore() {
+    scoreHome = 0
+    scoreGuest = 0
+    homeEl.textContent = scoreHome
+    guestEl.textContent = scoreGuest
+    homeEl.style.color = "#CC360F"
+    guestEl.style.color = "#CC360F"
+}
