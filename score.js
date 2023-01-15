b4fa22efca1608ad4b1f33b7e42a13651610033b
Martin Štiglinc <martin.stiglinc@gmail.com>
2023-01-15 15:29:17 +0100

    OneFunkc istead 6

--- score.js
+++ score.js
@@ -3,37 +3,48 @@ let scoreHome = 0
 let guestEl = document.getElementById("guest-el")
 let homeEl = document.getElementById("home-el")
 
-function addG1(){
-    scoreGuest += 1
-    guestEl.textContent = scoreGuest
+function addScore(team, score){
+    if(team = "guest"){
+        scoreGuest += score
+        guestEl.textContent = scoreGuest
+    } else {
+        scoreHome += score
+        homeEl.textContent = scoreHome 
+    }
     colors()
 }
 
-function addG2(){
-    scoreGuest += 2
-    guestEl.textContent = scoreGuest
-    colors()
-}
-function addG3(){
-    scoreGuest += 3
-    guestEl.textContent = scoreGuest
-    colors()
-}
-function addH1(){
-    scoreHome += 1
-    homeEl.textContent = scoreHome
-    colors()
-}
-function addH2(){
-    scoreHome += 2
-    homeEl.textContent = scoreHome
-    colors()
-}
-function addH3(){
-    scoreHome += 3
-    homeEl.textContent = scoreHome
-    colors()
-}
+// function addG1(){
+//     scoreGuest += 1
+//     guestEl.textContent = scoreGuest
+//     colors()
+// }
+
+// function addG2(){
+//     scoreGuest += 2
+//     guestEl.textContent = scoreGuest
+//     colors()
+// }
+// function addG3(){
+//     scoreGuest += 3
+//     guestEl.textContent = scoreGuest
+//     colors()
+// }
+// function addH1(){
+//     scoreHome += 1
+//     homeEl.textContent = scoreHome
+//     colors()
+// }
+// function addH2(){
+//     scoreHome += 2
+//     homeEl.textContent = scoreHome
+//     colors()
+// }
+// function addH3(){
+//     scoreHome += 3
+//     homeEl.textContent = scoreHome
+//     colors()
+// }
 
 function colors(){
     if(scoreHome>scoreGuest){
